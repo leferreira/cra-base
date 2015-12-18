279ea4fb6dd15d51b9cb3513bb13e5506ffe3a39
leandro <leandro@lefer>
2015-12-18 10:23:54 -0300

    ajuste em entidades

--- src/main/java/br/com/ieptbto/cra/entidade/TituloCnp.java
+++ src/main/java/br/com/ieptbto/cra/entidade/TituloCnp.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.envers.Audited;
@@ -64,12 +66,14 @@ public class TituloCnp extends AbstractEntidade<TituloCnp> {
 
 	@Override
 	@Id
-	@Column(name = "ID_TITULO_CNP", columnDefinition = "serial")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_TITULO_CNP", columnDefinition = "serial")
 	public int getId() {
 		return this.id;
 	}
 
+	@ManyToOne
+	@JoinColumn(name = "REMESSA_ID", columnDefinition = "integer not null")
 	public RemessaCnp getRemessa() {
 		return remessa;
 	}
