3f0f34839d8552bbf00186b93bd93ca9cdb8e209
Leandro <leferfsa@gmail.com>
2016-08-08 08:52:22 -0300

    correção de nullpointer na regra de verificação de cabeçalho

--- src/main/java/br/com/ieptbto/cra/enumeration/TipoArquivoEnum.java
+++ src/main/java/br/com/ieptbto/cra/enumeration/TipoArquivoEnum.java
@@ -8,55 +8,58 @@ import br.com.ieptbto.cra.exception.InfraException;
 @Entity
 public enum TipoArquivoEnum implements AbstractCraEnum {
 
-												REMESSA("B", "TPR", "Remessa"),
-												CONFIRMACAO("C", "CRT", "Confirmação"),
-												RETORNO("R", "RTP", "Retorno"),
-												CANCELAMENTO_DE_PROTESTO("CP", "", "Cancelamento"),
-												DEVOLUCAO_DE_PROTESTO("DP", "", "Desistência"),
-												AUTORIZACAO_DE_CANCELAMENTO("AC", "", "Autorização");
+    REMESSA("B", "TPR", "Remessa"), CONFIRMACAO("C", "CRT", "Confirmação"), RETORNO("R", "RTP", "Retorno"), CANCELAMENTO_DE_PROTESTO("CP",
+            "", "Cancelamento"), DEVOLUCAO_DE_PROTESTO("DP", "", "Desistência"), AUTORIZACAO_DE_CANCELAMENTO("AC", "", "Autorização");
 
-	public String constante;
-	public String identificacaoTransacaoCabecalho;
-	public String label;
+    public String constante;
+    public String identificacaoTransacaoCabecalho;
+    public String label;
 
-	TipoArquivoEnum(String constante, String identificacaoTransacaoCabecalho, String label) {
-		this.constante = constante;
-		this.identificacaoTransacaoCabecalho = identificacaoTransacaoCabecalho;
-		this.label = label;
-	}
+    TipoArquivoEnum(String constante, String identificacaoTransacaoCabecalho, String label) {
+        this.constante = constante;
+        this.identificacaoTransacaoCabecalho = identificacaoTransacaoCabecalho;
+        this.label = label;
+    }
 
-	/**
-	 * retorna o tipo de arquivo dependendo do tipo informado
-	 * 
-	 * @param valor
-	 * @return tipo arquivo
-	 */
-	public static TipoArquivoEnum getTipoArquivoEnum(String valor) {
-		TipoArquivoEnum[] values = TipoArquivoEnum.values();
-		for (TipoArquivoEnum tipoArquivo : values) {
-			if (valor.startsWith(tipoArquivo.getConstante())) {
-				return tipoArquivo;
-			}
-		}
-		throw new InfraException(Erro.TIPO_DE_ARQUIVO_NAO_ENCONTRADO.getMensagemErro() + valor);
-	}
+    /**
+     * retorna o tipo de arquivo dependendo do tipo informado
+     * 
+     * @param valor
+     * @return tipo arquivo
+     */
+    public static TipoArquivoEnum getTipoArquivoEnum(String valor) {
+        TipoArquivoEnum[] values = TipoArquivoEnum.values();
+        for (TipoArquivoEnum tipoArquivo : values) {
+            if (valor.startsWith(tipoArquivo.getConstante())) {
+                return tipoArquivo;
+            }
+        }
+        throw new InfraException(Erro.TIPO_DE_ARQUIVO_NAO_ENCONTRADO.getMensagemErro() + valor);
+    }
 
-	@Override
-	public String getConstante() {
-		return constante;
-	}
+    @Override
+    public String getConstante() {
+        return constante;
+    }
 
-	@Override
-	public String getLabel() {
-		return this.label;
-	}
+    @Override
+    public String getLabel() {
+        return this.label;
+    }
 
-	public String getIdentificacaoTransacaoCabecalho() {
-		return identificacaoTransacaoCabecalho;
-	}
+    public String getIdentificacaoTransacaoCabecalho() {
+        return identificacaoTransacaoCabecalho;
+    }
 
-	@Override
-	public String toString() {
-		return constante + " - " + label.toUpperCase();
-	}
+    @Override
+    public String toString() {
+        return constante + " - " + label.toUpperCase();
+    }
+
+    public boolean equals(TipoArquivoEnum tipoArquivo) {
+        if (this.getConstante().equals(tipoArquivo.getConstante())) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/br/com/ieptbto/cra/validacao/RegraVerificarCidadeCodigoIBGE.java
+++ src/main/java/br/com/ieptbto/cra/validacao/RegraVerificarCidadeCodigoIBGE.java
@@ -15,28 +15,36 @@ import br.com.ieptbto.cra.validacao.regra.RegraCabecalho;
 @Service
 public class RegraVerificarCidadeCodigoIBGE extends RegraCabecalho {
 
-	@Autowired
-	private MunicipioMediator municipioMediator;
+    @Autowired
+    private MunicipioMediator municipioMediator;
 
-	public void executar(CabecalhoRemessa cabecalho, List<Exception> erros) {
-		setErros(erros);
-		setCabecalhoRemessa(cabecalho);
-		executar();
+    public void executar(CabecalhoRemessa cabecalho, List<Exception> erros) {
+        setErros(erros);
+        setCabecalhoRemessa(cabecalho);
+        executar();
 
-	}
+    }
 
-	@Override
-	protected void executar() {
-		Municipio municipio = municipioMediator.buscaMunicipioPorCodigoIBGE(getCabecalhoRemessa().getCodigoMunicipio());
-		if (municipio == null) {
-			throw new InfraException("Código do Município " + getCabecalhoRemessa().getCodigoMunicipio() + " não encontrado ou inválido!");
-		}
+    @Override
+    protected void executar() {
+        Municipio municipio = municipioMediator.buscaMunicipioPorCodigoIBGE(getCabecalhoRemessa().getCodigoMunicipio());
+        if (municipio == null) {
+            throw new InfraException("Código do Município " + getCabecalhoRemessa().getCodigoMunicipio() + " não encontrado ou inválido!");
+        }
 
-		if (!TipoArquivoEnum.REMESSA.equals(getCabecalhoRemessa().getRemessa().getArquivo().getTipoArquivo().getTipoArquivo())) {
-			Municipio municipioEnvio = municipioMediator.carregarMunicipio(getCabecalhoRemessa().getRemessa().getInstituicaoOrigem().getMunicipio());
-			if (!municipio.equals(municipioEnvio)) {
-				throw new InfraException("Código do Município no cabeçalho não pertence a instituição que está enviando o arquivo!");
-			}
-		}
-	}
+        if (!TipoArquivoEnum.REMESSA.equals(getCabecalhoRemessa().getRemessa().getArquivo().getTipoArquivo().getTipoArquivo())) {
+
+            if (getCabecalhoRemessa().getRemessa().getInstituicaoOrigem() != null
+                    && getCabecalhoRemessa().getRemessa().getInstituicaoOrigem().getMunicipio() != null) {
+                Municipio municipioEnvio = municipioMediator
+                        .carregarMunicipio(getCabecalhoRemessa().getRemessa().getInstituicaoOrigem().getMunicipio());
+
+                if (!municipio.equals(municipioEnvio)) {
+                    throw new InfraException("Código do Município no cabeçalho não pertence a instituição que está enviando o arquivo!");
+                }
+            }
+
+            throw new InfraException("Código do Município no cabeçalho não pertence a instituição que está enviando o arquivo!");
+        }
+    }
 }
