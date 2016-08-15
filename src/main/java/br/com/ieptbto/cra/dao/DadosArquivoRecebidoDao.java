a80297bc30039eb838003d6eaa5bc9eecd9a8673
Leandro <leferfsa@gmail.com>
2016-08-15 10:13:14 -0300

    processador de arquivos recebidos pelo WS

---
+++ src/main/java/br/com/ieptbto/cra/dao/DadosArquivoRecebidoDao.java
@@ -0,0 +1,37 @@
+package br.com.ieptbto.cra.dao;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.ieptbto.cra.entidade.DadosArquivoRecebido;
+import br.com.ieptbto.cra.exception.InfraException;
+
+/**
+ * 
+ * @author Leandro
+ *
+ */
+@Repository
+public class DadosArquivoRecebidoDao extends AbstractBaseDAO {
+
+    /**
+     * Método responsável por persistir os dadosRecebidos pelo WS na base de
+     * dados
+     * 
+     * @param dadosArquivoRecebido
+     * @return
+     */
+    public DadosArquivoRecebido salvar(DadosArquivoRecebido dadosArquivoRecebido) {
+        try {
+
+            return save(dadosArquivoRecebido);
+
+        } catch (Exception ex) {
+            new InfraException("Não foi possível inserir os dados recebidos pelo WS na base de dados");
+            logger.error(ex.getMessage(), ex);
+        }
+
+        return null;
+
+    }
+
+}
